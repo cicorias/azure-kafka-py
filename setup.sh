ac2cc69354f11f9bb63af043b8dd530b8b856c9b
cicorias <github@cicoria.com>
2019-02-03 03:13:04 +0100

    update

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 echo "Downloading/installing necessary dependencies"
-sudo apt-get install git openssl libssl-dev build-essential python-pip python-dev librdkafka-dev
-git clone https://github.com/edenhill/librdkafka
+sudo apt-get install git openssl libssl-dev build-essential python-pip python-dev
+# these steps should not be neded as the confluent-kafka package installs
+# respective libs (dll, so, a) for each platform 
+# git clone https://github.com/edenhill/librdkafka
 
-echo "Setting up librdkafka"
-cd librdkafka
-./configure
-make
-sudo make install
-cd ..
+# echo "Setting up librdkafka"
+# cd librdkafka
+# ./configure
+# make
+# sudo make install
+# cd ..
 
 echo "Setting up Confluent's Python Kafka library"
 sudo pip install confluent-kafka
